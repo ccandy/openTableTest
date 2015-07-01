faebdba8b17f8940aa71c27e7af39fbb214bda39
Yidai Corp <yidaicorp@YidaideiMac.local>
2015-06-30 18:03:58 -0700

    Initial Commit

---
+++ openTableTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  openTableTest
+//
+//  Created by Yidai Corp. on 15/6/30.
+//  Copyright (c) 2015年 Yidai Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
